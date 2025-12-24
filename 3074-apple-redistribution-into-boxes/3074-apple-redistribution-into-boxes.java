82313cf1afd43f64ddbb4362750b24383148fb85
Tirumanisetti Srinu <srinutirumanisetti@gmail.com>
2025-12-24 21:29:15 +0530

    Time: 4 ms (82.79%), Space: 44.2 MB (79.53%) - LeetHub

---
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        Arrays.sort(capacity);
+        int sum=0;
+        for(int i=0;i<apple.length;i++){
+            sum+=apple[i];
+        }
+        int count=0;
+        for(int i=capacity.length-1;i>=0;i--){
+            if(sum<=0){
+                break;
+            }
+            else{
+                sum-=capacity[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
